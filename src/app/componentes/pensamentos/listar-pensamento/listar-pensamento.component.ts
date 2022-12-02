7bd619e51f1f3e289a855c76cb70f6d39c3113ad
Eduardo Silva <eduardosilva@lampp-it.com.br>
2022-12-02 09:39:22 -0300

    pesquisa por input

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,9 @@
 import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { filter, map, Observable, of, take, tap, throwIfEmpty } from 'rxjs';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -12,25 +15,45 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   haMaisPensamentos: boolean = true;
   paginaAtual: number = 1;
+  filtro: string = '';
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.service.listar(1).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
+    this.pegaListaDePensamentos();
+  }
+
+  pegaListaDePensamentos() {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos;
+    });
   }
 
   carregarMaisPensamentos(): void {
-    this.service.listar(++this.paginaAtual).subscribe({
-      next: novosPensamentos => {
-        this.listaPensamentos.push(...novosPensamentos);
-        if (!novosPensamentos.length) {
-          this.haMaisPensamentos = false;
-        }
-      },
+    this.service.listar(++this.paginaAtual, this.filtro)
+      .pipe(take(1))
+      .subscribe({
+        next: novosPensamentos => {
+          this.listaPensamentos.push(...novosPensamentos);
+          if (!novosPensamentos.length) {
+            this.haMaisPensamentos = false;
+          }
+        },
+        error: err => console.log(err.error)
+      })
+  }
+
+  pesquisarPensamentos(): void {
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro).subscribe({
+      next: response => this.listaPensamentos = response,
       error: err => console.log(err.error)
     })
   }
+
+
 }
 
